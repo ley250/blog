476e327a645691baf9a2dcf9510acf27a6d9de68
TH_tarot <ley250@gmail.com>
2016-07-31 17:44:06 +0900

    add 2016-07-09

---
+++ gitpush.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git status
+
+echo -en '\n\ncommit message : '
+read msg
+
+git add -A
+git commit -m "$msg"
+
+echo -en '\n\npush?(y/n) : '
+read input
+
+if [ $input == "y" -o $input == "Y" ]
+then
+	git push origin master
+else
+	echo -e "\ndon't push."
+fi
